d497e8891636f34364bcb2a0b4818a91bf5c1454
YOUR_GITHUB_USERNAME <garrickdark@yahoo.com>
2017-07-11 15:38:30 -0700

    created simple race game where you get from point A to B and whoever gets there first wins

--- example-readme.md
+++ example-readme.md
@@ -1,21 +1,24 @@
 # Racing Game
+*created a simple racing game featuring a ferrari(colored in red) and a lamborghini(colored in green)
 
-*Short explanation of the project (1-2 sentences) - what is the goal of the project and/or main functionality?*
 
-## Technologies Used
+# Technologies Used
+*
+
 
 *What technologies did you use to develop this project? (bullet points)*
 
 ## Existing Features
+*players can move from one end to the end and have a game end when a player reaches the other side.
+*added a reset button with non-functionality yet
+
 
 *What features does your racing game have? (bullet points)*
+
+
 
 ## Planned Features
 
 *What changes would you make to your project if you continue to work on it? (bullet points)*
 
 ---
-
-##### Screenshot (optional)
-
-##### Link to hosted project on GitHub pages or BitBalloon (optional)
---
+++ scripts/app.js
@@ -0,0 +1,88 @@
+console.log("sanity check")
+
+// function Car(type, color) {
+// 	this.type = type;
+// 	this.color = color;
+// 	this.move = function(){};
+// }
+//
+// var ferrari = new Car("ferrari", "red");
+// var lamborghini = new Car("lamborghini", "black");
+//
+//
+//
+// //How would you make your player's "cars" use custom images?
+//   link a image type to the image image sub-object
+//   ie: this.image = "url to image"
+//
+// //how to make car move
+// have a listener that waits for a "click" then move X pixels or spaces?
+//
+// //Can a player type in their name to see custom win messages?
+// link 2 use instances to input commands
+// set name to input text
+// set win to "`${user-name} has won!`"
+//
+// //Can you enable a reset button to restart the race?
+// cmmd-R
+//
+// //How about a win counter that spans across multiple races?
+// if player A wins countA ++;
+// if player B wins countB ++;
+//
+// link a score counter to countA and countB
+
+$(function() {
+
+  // global variables
+  $confetti = $('#confetti');
+  $reset = $('#reset');
+  var winner = null;
+
+  // reset to initial state of race
+  var raceAgain = function() {
+    $confetti.css({'z-index': -1, opacity: 0});
+    $reset.addClass('tmp-hidden');
+    $('.player1').css({left: 0});
+    $('.player2').css({left: 0});
+    winner = null;
+  };
+
+  // show confetti and reset button when winner declared
+  var executeWin = function() {
+    $confetti.css({'z-index': 1, opacity: 1});
+    $reset.removeClass('tmp-hidden');
+  };
+
+  // take keypress event and move correct player
+  var movePlayer = function(keypressEvent) {
+    // returns key like 'a', 'l', etc.
+    var keyCode = String.fromCharCode(keypressEvent.keyCode);
+    // find player and player's position
+    var $player = $('[data-key="' + keyCode + '"]');
+    var leftPosition = $player.offset().left;
+    // set player's new position (move forward)
+    $player.css({left: leftPosition + 1});
+
+    // if player moved past end of track
+    if ($player.offset().left >= $('#track').width() - $player.width()) {
+      // set winner to player and execute win
+      winner = $player;
+      executeWin();
+    }
+  };
+  // add event-handlers
+  var race = function() {
+    $(window).on('keypress', function(event) {
+      if (!winner) {
+        movePlayer(event);
+      }
+    });
+    $reset.on('click', function() {
+      raceAgain();
+    });
+  };
+  // start the race!
+  race();
+
+});
